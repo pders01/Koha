ba144d2b304a034bdb463d4455051d11b7169822
LMSCloudPaulD <paul.derscheid@lmscloud.de>
2023-10-22 22:29:22 +0000

    Initial commit for move of cities to vue

--- koha-tmpl/intranet-tmpl/prog/js/vue/fetch/api-client.js
+++ koha-tmpl/intranet-tmpl/prog/js/vue/fetch/api-client.js
@@ -5,6 +5,7 @@ import AVAPIClient from "./authorised-values-api-client";
 import ItemAPIClient from "./item-api-client";
 import SysprefAPIClient from "./system-preferences-api-client";
 import PreservationAPIClient from "./preservation-api-client";
+import CitiesAPIClient from "./cities-api-client";
 
 export const APIClient = {
     erm: new ERMAPIClient(),
@@ -14,4 +15,5 @@ export const APIClient = {
     item: new ItemAPIClient(),
     sysprefs: new SysprefAPIClient(),
     preservation: new PreservationAPIClient(),
+    cities: new CitiesAPIClient(),
 };
