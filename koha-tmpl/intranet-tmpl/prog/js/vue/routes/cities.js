4fb548cfa28253acdff4cdd42914118ec7025590
LMSCloudPaulD <paul.derscheid@lmscloud.de>
2024-01-23 09:52:27 +0000

    Revert "Revert "Temporary backup of changes""
    This reverts commit 2e2861bb6eb38546439adea017da91bbc06660d4.

--- koha-tmpl/intranet-tmpl/prog/js/vue/routes/cities.js
+++ koha-tmpl/intranet-tmpl/prog/js/vue/routes/cities.js
@@ -1,43 +1,22 @@
-import Home from "../components/Cities/Home.vue";
+import { markRaw } from "vue";
 
-const breadcrumbs = {
-    home: {
-        text: "Home", // $t("Home")
-        path: "/cgi-bin/koha/mainpage.pl",
-    },
-    admin_home: {
-        text: "Administration", // $t("Administration")
-        path: "/cgi-bin/koha/admin/admin-home.pl",
-    },
-    cities: {
-        text: "Cities", // $t("Cities")
-        path: "/cgi-bin/koha/admin/cities.pl",
-    },
-};
+import Main from "../components/Cities/Main.vue";
+
+import { $__ } from "../i18n";
 
 export const routes = [
     {
-        path: "/cgi-bin/koha/mainpage.pl",
-        beforeEnter() {
-            window.location.href = "/cgi-bin/koha/mainpage.pl";
-        },
-    },
-    {
-        path: "/cgi-bin/koha/admin/admin-home.pl",
-        beforeEnter() {
-            window.location.href = "/cgi-bin/koha/admin/admin-home.pl";
-        },
-    },
-    {
         path: "/cgi-bin/koha/admin/cities.pl",
-        name: "Home",
-        component: Home,
-        meta: {
-            breadcrumb: () => [
-                breadcrumbs.home,
-                breadcrumbs.admin_home,
-                breadcrumbs.cities,
-            ],
-        },
+        is_default: true,
+        is_base: true,
+        title: $__("Cities"),
+        children: [
+            {
+                path: "",
+                name: "Home",
+                component: markRaw(Main),
+                is_navigation_item: false,
+            },
+        ],
     },
 ];
